f0d4b006882345f38f322e71adc4d73f715a395d
Juma Kibula <98959076+Aka-Jr@users.noreply.github.com>
2023-08-29 15:38:02 +0300

    Landing/Home page
    An overview of the home page

--- elimu-fund/src/App.js
+++ elimu-fund/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+// Slider os the name of the class and slider is the name of the file
+import NavBar from './NavBar';
+import './styles.css';
+import Home from './pages/home';
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  return(
+    <div>  
+      <NavBar />
+      <Home />
 
+    </div>
+  )
+  
+ 
+  
+  // <Home />
+  
+}
 export default App;
---
+++ elimu-fund/src/pages/About.js
@@ -0,0 +1,7 @@
+import NavBar from "../NavBar";
+
+const Home = () => {
+    return <h1>Welcome at About Us Page</h1>;
+}
+
+export default Home;
